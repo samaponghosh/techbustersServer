4892d4b7636b55b732a46ea5b16797ce5759885f
samaponghosh <samaponghosh2002@gmail.com>
2024-05-06 16:18:43 +0530

    env comm

--- src/jwtProvider/jwtProvider.js
+++ src/jwtProvider/jwtProvider.js
@@ -1,8 +1,8 @@
-// require('dotenv').config();
+require('dotenv').config();
 const jwt = require('jsonwebtoken')
 
-// const SECRET_KEY = process.env.SECRET_KEY
-const SECRET_KEY = "balerkwvjbdvouwhbuow"
+const SECRET_KEY = process.env.SECRET_KEY
+// const SECRET_KEY = "balerkwvjbdvouwhbuow"
 const generateToken = (userId)=>
 {
     const token = jwt.sign({userId},SECRET_KEY,{expiresIn: '24h' })
